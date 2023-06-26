7c8c7268063801d54c61195d4816dc31e9ae5d48
Polly2000 <kuznetsovapolina2020@gmail.com>
2023-06-27 00:15:26 +0300

    feat: lesson 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              <PizzaBlock price={349} title="Мексиканская" />
+              <PizzaBlock price={449} title="Гавайская" />
+              <PizzaBlock price={250} title="Сырная" />
+              <PizzaBlock price={400} title="Острая" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
