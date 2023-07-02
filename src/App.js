09548c6c6a7cc12ce198481ac53b5cd66117789b
Polly2000 <kuznetsovapolina2020@gmail.com>
2023-07-02 23:08:45 +0300

    feat: lesson 11 (context)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
@@ -7,21 +7,25 @@ import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
 import './scss/app.scss';
 
+export const SearchContext = createContext('');
+
 function App() {
   const [searchValue, setSearchValue] = useState('');
 
   return (
     <div className="App">
-      <div className="wrapper">
-        <Header searchValue={searchValue} setSearchValue={setSearchValue} />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home searchValue={searchValue} />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+        <div className="wrapper">
+          <Header />
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </SearchContext.Provider>
     </div>
   );
 }
